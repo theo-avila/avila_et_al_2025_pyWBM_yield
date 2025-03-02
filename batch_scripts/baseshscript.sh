8ab17205d6101ce6e337118014ddc600bef73998
theo-avila <ctavila2@illinois.edu>
2025-03-02 00:18:53 -0500

    remove unnecessary files, clean up repo

--- batch_scripts/baseshscript.sh
+++ batch_scripts/baseshscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
-#SBATCH --job-name=download_MOSAIC
-#SBATCH --output=MOSAIC%j.log
+#SBATCH --job-name=
+#SBATCH --output=
 #SBATCH --partition=pches
 #SBATCH --nodes=1                
 #SBATCH --ntasks=1
@@ -16,6 +16,6 @@ conda activate pyWBM || { echo "Failed to activate pyWBM env"; exit 1; }
 
 # Run the Python script
 echo "Running Python script"
-python /storage/home/cta5244/work/avila_et_al_2025_pyWBM_yield/2a_data_processing_soilmoisture_mosaic.py || { echo "Python script execution failed"; exit 1; }
+python /storage/home/cta5244/work/avila_et_al_2025_pyWBM_yield/2a_data_processing_gdd.py || { echo "Python script execution failed"; exit 1; }
 echo "Job completed"
 
