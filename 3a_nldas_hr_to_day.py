f62fa8beb4e6b06d8145c1d0171363e18acb6ec4
theo-avila <ctavila2@illinois.edu>
2025-03-14 22:44:00 -0400

    broken averaging with dask. comm error being worked on.

--- 3a_nldas_hr_to_day.py
+++ 3a_nldas_hr_to_day.py
@@ -1,6 +1,6 @@
 import dask
 import xarray as xr
-import numpy 
+import numpy as np
 import pandas as pd
 import datetime
 import os 
@@ -13,7 +13,7 @@ def select_soilm(ds):
     '''
     return ds[['SoilM_0_100cm']]
     
-def average_hourly(start_dt, file_path_base, downloads_dir):
+def average_hourly(start_dt, file_path_base, year_downloads_dir):
     '''
     takes base file path and averages from hourly to daily using xarray resampling. Also creates dir for files
     inputs:
@@ -21,16 +21,24 @@ def average_hourly(start_dt, file_path_base, downloads_dir):
     outputs:
     daily average in appropriate subdirectory
     '''
-    os.makedirs(downloads_dir, exist_ok=True)
-    current_dt = start_dt
-    yyyymmdd = current_dt.strftime("%Y%m%d")
-    
+    os.makedirs(year_downloads_dir, exist_ok=True)
+    yyyymmdd = start_dt.strftime("%Y%m%d")
     file_pattern = f"{file_path_base}{yyyymmdd}.*.020.nc"
-    ds = xr.open_mfdataset(file_pattern, combine='by_coords', preprocess=select_soilm)
-    daily_soilm = ds.resample(time='1D').mean()
 
-    output_path = os.path.join(downloads_dir, f"NLDAS_0_100_soilm_{NLDAS_lsm}0125_H.A{yyyymmdd}.020.nc")
-    daily_soilm.to_netcdf(output_path)
+    try:
+        ds = xr.open_mfdataset(file_pattern, combine='by_coords')
+        
+        daily_soilm = ds.resample(time='1D').mean(skipna=True)['SoilM_0_100cm']
+        
+        output_path = os.path.join(year_downloads_dir, f"NLDAS_0_100_soilm_{NLDAS_lsm}0125_H.A{yyyymmdd}.020.nc")
+        
+        daily_soilm.compute().to_netcdf(output_path)
+        
+    except OSError:
+        print(f"No files to open for pattern: {file_pattern}. Skipping this date.")
+    except Exception as e:
+        print(f"Unexpected error processing {yyyymmdd}: {e}")
+    
 
 def day_iteration(year, file_path_base, downloads_dir):
     '''
@@ -40,16 +48,18 @@ def day_iteration(year, file_path_base, downloads_dir):
     file_path_base
     '''
     start_dt = datetime.datetime(year, 1, 1, 0, 0)
-    end_dt = datetime.datetime(year, 1, 2, 0, 0)
+    end_dt = datetime.datetime(year, 12, 31, 0, 0)
+    year_downloads_dir = (f"{downloads_dir}/{year}")
+    
     while start_dt <= end_dt:
-        average_hourly(start_dt, file_path_base, downloads_dir)
+        average_hourly(start_dt, file_path_base, year_downloads_dir)
         start_dt += datetime.timedelta(days=1)
 
 
-NLDAS_lsm = 'MOS'
+NLDAS_lsm = 'VIC'
 file_path_base = f"/storage/group/pches/default/public/NLDAS/{NLDAS_lsm}/hourly/NLDAS_{NLDAS_lsm}0125_H.A"
 downloads_dir = f"/storage/home/cta5244/work/pyWBM_yield_data/{NLDAS_lsm}_daily/"
-
+start_year, end_year = 1979, 2025
 
 from dask_jobqueue import SLURMCluster
 
@@ -57,21 +67,19 @@ cluster = SLURMCluster(
     # account="pches",
     account="open",
     cores=1,
-    memory="8GiB",
+    memory="4GiB",
     walltime="05:00:00",
 )
 
-cluster.scale(jobs=10) 
+cluster.scale(jobs=20)
 
 from dask.distributed import Client
 
 client = Client(cluster)
 
-
 results = []
 for year in np.arange(start_year, end_year, 1):
     out = dask.delayed(day_iteration)(year=year, file_path_base=file_path_base, downloads_dir=downloads_dir)
     results.append(out)
-
 
 results = dask.compute(*results)
