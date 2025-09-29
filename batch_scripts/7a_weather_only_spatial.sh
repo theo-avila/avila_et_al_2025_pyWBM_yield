05b4a1155c2deebff52fc794230ca88635c38d54
theo-avila <ctavila2@illinois.edu>
2025-09-29 19:56:16 -0400

    nxn panel year chunk shocks

---
+++ batch_scripts/7a_weather_only_spatial.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --job-name=7a_weather_only_spatial
+#SBATCH --output=7a_weather_only_spatial.log
+#SBATCH --partition=open
+#SBATCH --nodes=1                
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1     
+#SBATCH --mem=250GB                
+#SBATCH --time=24:00:00
+
+echo "Sourcing Conda"
+source $HOME/mambaforge/etc/profile.d/conda.sh || { echo "Failed to source conda.sh"; exit 1; }
+echo "Activating WFPIenv"
+conda activate pyWBM || { echo "Failed to activate pyWBM env"; exit 1; }
+
+echo "Running Python script"
+python /storage/home/cta5244/work/avila_et_al_2025_pyWBM_yield/7a_weather_only_spatial.py || { echo "Python script execution failed"; exit 1; }
+echo "Job completed"
+
